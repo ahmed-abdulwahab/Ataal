81141f7a76061a2a13ea29d71d87245e0f70f997
ahmed-abdulwahab <ahmedmuhammed2420@gmail.com>
2023-04-16 23:59:00 +0200

    View All the problems Based on the Technical's section and Technical's Blocked List of Customers.

---
+++ ApI_project/Controllers/problem/ProblemController.cs
@@ -0,0 +1,26 @@
+﻿using Ataal.BL.Managers.problem;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ataal.Controllers.problem
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProblemController : ControllerBase
+    {
+        private readonly IProblemManager _problemManager;
+        public ProblemController(IProblemManager problemManager)
+        {
+            _problemManager= problemManager;
+        }
+        [HttpGet]
+        public IActionResult GetAllProblems(int TechnicalID)
+        {
+            var problems=_problemManager.GetProblemsForTechnical(TechnicalID);
+            if (problems == null)
+                return BadRequest();
+            else
+                return Ok(problems);
+        }
+    }
+}
