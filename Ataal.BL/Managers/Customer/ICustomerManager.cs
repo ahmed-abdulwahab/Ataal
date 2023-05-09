4567b92007cb6c3332f89b92b6b3849e69b76433
ahmed-abdulwahab <ahmedmuhammed2420@gmail.com>
2023-05-09 19:50:06 +0300

    GetAdminByAppUser

--- Ataal.BL/Managers/Customer/ICustomerManager.cs
+++ Ataal.BL/Managers/Customer/ICustomerManager.cs
@@ -20,6 +20,8 @@ namespace Ataal.BL.Managers.Customer
     public interface ICustomerManager
     {
         public LoginCustomerDto? GetCustomerByAppUser(string Appuser);
+        public LoginCustomerDto? GetAdminByAppUser(string Appuser);
+
         public LoginCustomerDto? GetTechByAppUser(string Appuser);
         public NotificationDto GetAllNotification(int CustomerId);
         public int GetNotificationCount(int CustomerId);
